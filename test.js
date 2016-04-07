e825096dfb08c61574ca238fcbf15f3ac91d61a7
Sindre Sorhus <sindresorhus@gmail.com>
2016-04-07 14:13:14 +0700

    upgrade to latest AVA

--- test.js
+++ test.js
@@ -3,5 +3,5 @@ import test from 'ava';
 import m from './';
 
 test(async t => {
-	t.same(await m(path.join(__dirname, 'fixture')), ['default', 'test']);
+	t.deepEqual(await m(path.join(__dirname, 'fixture')), ['default', 'test']);
 });
